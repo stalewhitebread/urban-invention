7e092d292d06316fc0b299acd293e7c8310557a3
stalewhitebread <111912670+stalewhitebread@users.noreply.github.com>
2022-09-08 14:51:02 -0500

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -2,13 +2,13 @@ $(document).ready(function(){
   //click on image, get message
   $("#dogeOne").click(function(){
     console.log("User clicked dogeOne")
-  $("#outputDiv").toggle();
+  $("#outputDiv").toggle().fadeIn();
   $("#outputMsg").text("Wow! Such Doge!");
   
   });
    $("#dogeTwo").click(function(){
     console.log("User clicked dogeTwo")
-    $("#outputDiv").toggle();
+    $("#outputDiv").toggle().fadeIn();
   $("#outputMsg").text("Loser! Loser! loser loser loser~")
   });
 });
